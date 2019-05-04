c0bf0c564f17c5234625eaf5cb3f59744697e95f
fon_harry <fon.harry@gmail.com>
2019-05-04 09:42:30 +0300

    Improved inserting to database

--- index.js
+++ index.js
@@ -2,16 +2,30 @@ const environment = process.env.NODE_ENV || "development";
 const configuration = require("./knexfile")[environment];
 const database = require("knex")(configuration);
 
-const data = require("./data.json");
+const data = require("./data_real.json");
 
 Promise.all(
   data.managersDebts.map(({ id: id_8, name, clients }) => {
     return database("managers")
       .insert({ id_8, name })
       .then(manager_id => {
-        Promise.all(
+        return Promise.all(
           clients.map(({ id: id_8, name, contracts }) => {
-            return database("clients").insert({ manager_id, id_8, name });
+            return database("clients")
+              .insert({ manager_id, id_8, name })
+              .then(client_id => {
+                return Promise.all(
+                  contracts.map(({ id: id_8, name, debt }) => {
+                    return database("contracts").insert({
+                      client_id,
+                      id_8,
+                      name,
+                      debt,
+                      decimal_debt: debt
+                    });
+                  })
+                );
+              });
           })
         );
       });
